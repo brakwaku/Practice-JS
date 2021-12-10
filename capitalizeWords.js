f7e55d44e130a69d08210524cf9a483c01d342fe
Kwaku Appau-Nkansah <brakwakujr@gmail.com>
2021-12-09 17:13:09 -0700

    Edit capitalize

--- capitalizeWords.js
+++ capitalizeWords.js
@@ -1,12 +1,3 @@
-// const capitalize = (sentence) => {
-//     const words = [];
-//     for (let word of sentence.split(' ')) {
-//         words.push(word[0].toUpperCase() + word.slice(1));
-//     }
-
-//     console.log(words.join(' '));
-// }
-
 const capitalize = (sentence) => {
     const words = [];
     for (let word of sentence.split(' ')) {
@@ -16,4 +7,18 @@ const capitalize = (sentence) => {
     console.log(words.join(' '));
 }
 
-capitalize('Kofi is a boy. But kofi is a toy');
+// const capitalize = (sentence) => {
+//     let result = sentence[0].toUpperCase();
+
+//     for (let i = 1; i < sentence.lenght; i++) {
+//         if (sentence[i - 1] === ' ') {
+//             result += sentence[i].toUpperCase();
+//         } else {
+//             result += sentence[i];
+//         }
+//     }
+
+//     console.log(result);
+// }
+
+capitalize('kofi is a boy. But kofi is a toy');
